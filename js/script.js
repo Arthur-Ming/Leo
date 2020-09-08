bc4424aa2ab179ec56153946082c46feabac9e86
Arthur-Ming <arthurming7@gmail.com>
2020-09-08 14:59:27 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,65 +1,4 @@
-$(document).ready(function () {
-
-  $('.slider').slick({
-    arrows: true,         /*стрелки, по умолчанию true*/
-    dots: true,           /*точки, по умолчанию false*/
-    adaptiveHeight: true, /*адаптивная высота, поумл false*/
-    slidesToShow: 3,      /*кол-во отоброжаемых слайдов */
-    slidesToScroll: 1,    /*кол-во пролистывается за одно нажатие*/
-    speed: 1000,           /*скорость пролистывания поумл == 300*/
-    easing: 'ease',       /*сценарий анимации поумл linear*/
-    infinite: true,      /*бесконечный слайдер поумл true*/
-    initialSlide: 0,      /*стартовый слайд поумл 0*/
-    autoplay: false,       /*автопролистование*/
-    autoplaySpeed: 1500,  /*скорость автопролистования*/
-    pauseOnFocus: false,  /*пауза autoplay если слайд в фокусе*/
-    pauseOnHover: true,   /*пауза autoplay если на слайде курсор */
-    pauseOnDotsHover: true, /*пауза autoplay если на кнопке курсор*/
-    draggable: true,              /*вкл/выкл листание слайдера курсором, по умл true*/
-    swipe: true,                  /*вкл/выкл листание слайдера курсором на тачскринах*/
-    touchThreshold: 5,            /*отвечает за момент срабатывания swipe, поумл == 5*/
-    touchMove: false,              /*вкл/выкл возможность тянуть слайд при swipe*/
-    waitForAnimate: false,          /*если выкл то можно быстро листать слайдер, поумл true*/
-    centerMode: false,              /*центрирует слайд*/
-    variableWidth: false,           /*автоширина слайда, слайды разной ширины поумл false*/
-    rows: 1,                        /*число рядов поумл 1*/
-    slidesPerRow: 1,                  //число слайдов в ряду поумл 1
-    vertical: false,                   //вертикальный слайдер
-    verticalSwiping: false,            //прокрутка верт слайдера
-    //asNavFor: ".sliderbig",            //связывает слайдеры
-    responsive: [                      //адаптив
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ],
-    //appendArrows: $('.content'),  //перенос стрелок/кнопак в другой блок
-    // appendDots: $('.content'),
-
-  });
-  /*======события сладера=====*/
-  $('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-
-    console.log(nextSlide);
-  });
-  /*
-   $('.sliderbig').slick({
-        arrows: false,
-        fade:true,                 //слайды не листаются, а заменяются
-        asNavFor: ".slider",
- 
-   });
-   */
-});
-
+/* ==== Слайдеры === */
 $(document).ready(function () {
 
   $('.banner-slider').slick({
@@ -69,12 +8,41 @@ $(document).ready(function () {
     centerMode: true,
     infinite: false,
     centerPadding: '10px',
-
-
   });
+});
+/* ==== Табы === */
+$('.main-tabs__links').on('click', '.main-tabs__link', function (event) {
+
+  $('.main-tabs__link').removeClass('active');
+  $(this).addClass('active');
+
+  $('.main-tabs__content').removeClass('active');
+  $('#' + $(this).attr('data-link')).addClass('active');
 
 });
 
+$('.main-tabs__links').on('mousedown', function (event) {
+  event.preventDefault();
+});
+/* ==== === */
+function ibg() {
+  $.each($('.ibg'), function (index, val) {
+    if ($(this).find('img').length > 0) {
+      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+    }
+  });
+}
+
+ibg();
+/* ==== Бургер === */
+$('.menu-header__burger').on('click', function (event) {
+  $('.header-menu').toggleClass('active');
+  $('.menu-header__burger').toggleClass('bl');
+  $('body').toggleClass('lock');
+});
+
+
+/*
 document.querySelector(".main-tabs__links").addEventListener("click", function (event) {
 
   let target = event.target.closest(".main-tabs__link");
@@ -97,25 +65,12 @@ document.querySelector(".main-tabs__links").addEventListener("click", function (
   document.getElementById(target.dataset.link).classList.add("active");;
 
 });
+*/
 
+
+/*
 document.querySelector(".main-tabs__links").addEventListener('mousedown',
   function (event) {
     event.preventDefault();
   });
-
-
-function ibg() {
-  $.each($('.ibg'), function (index, val) {
-    if ($(this).find('img').length > 0) {
-      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-    }
-  });
-}
-ibg();
-
-$('.menu-header__burger').on('click', function (event) {
-  $('.header-menu').toggleClass('active');
-  $('.menu-header__burger').toggleClass('bl');
-  $('body').toggleClass('lock');
-
-});
+*/
